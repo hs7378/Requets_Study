af4a9bab5f1b45af09a6d60f181978e043bcdfa2
huashuai <hs7378@gmail.com>
2018-04-21 22:27:01 +0800

    20180418test

---
+++ requets_test1_HK/BS4-TEST.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import lxml
+import requests
+import re
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+}
+url = 'http://www.hkexnews.hk/sdw/search/mutualmarket_c.aspx?t=sh'
+r =  requests.get(url,headers = headers)
+print (r.status_code)
+html = r.text
+title = re.search('<title>(.*?)</title>',html,re.S)
+scoup = BeautifulSoup(html,'lxml')
+print(scoup.title.string)
+# print(scoup.head)
+# print(scoup.p)
+tables = scoup.findAll('table')
+tab = tables[2]
+for tr in tab.findAll('tr'):
+    for td in tr.findAll('td'):
+        print(td.getText())
